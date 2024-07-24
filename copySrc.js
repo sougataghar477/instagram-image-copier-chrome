88b7d7dbc5626d7cbd04f21eca4ce9b66ae45550
Sougata Ghar <50982886+sougataghar477@users.noreply.github.com>
2024-07-24 13:31:45 +0530

    Update copySrc.js

--- copySrc.js
+++ copySrc.js
@@ -1,30 +1,50 @@
-var c;
+//Context menu
 document.addEventListener("mousedown", (e) => {
   //get the class name
-  c = e.target.className;
+  var c = e.target.className;
   //check for right mouse click and if the class is there , remove the class
   if (e.target.classList.contains(c) && e.button === 2) {
     e.target.classList.remove(c);
   }
 });
 
-//copying image url on click
-document.addEventListener("click", (e) => {
-  var url = e.target.previousElementSibling.children[0].src;
-  navigator.clipboard.writeText(url);
-  const tooltip = document.createElement("SPAN");
-    tooltip.textContent = "copied";
-  tooltip.classList.add("tooltip");
-  e.target.appendChild(tooltip);
-  setTimeout(() => {
-    e.target.firstChild.remove();
-  }, 1500);
-});
 
-//enabling context menu
-document.addEventListener("contextmenu", (e) => {
-  if (e.target.tagName === "IMG") {
-    let blockedElement = e.target.parentElement.nextElementSibling;
-    blockedElement.classList.add(c);
+//Left Click
+document.addEventListener("click", async(e) => {
+  
+  function makeTooltip() {
+    const tooltip = document.createElement("SPAN");
+    tooltip.textContent = "copied";
+    tooltip.classList.add("tooltip");
+    return tooltip;  
   }
+
+  if (e.target.tagName === "DIV") {
+    
+    await navigator.clipboard.writeText(e.target.previousElementSibling.children[0].src)
+      .then(() => {
+        
+        let tooltip= makeTooltip();
+        
+        e.target.appendChild(tooltip);
+      
+        setTimeout(() => {
+          tooltip.remove();
+        }, 1500);
+      })
+      .catch(err=>{console.error(err)})
+  }
+
+if(e.target.tagName==="IMG"){
+  
+  await navigator.clipboard.writeText(e.target.src)
+  
+  let tooltip= makeTooltip();
+  
+  e.target.parentElement.appendChild(tooltip);
+
+  setTimeout(() => {
+    tooltip.remove();
+  }, 1500);
+ }
 });
