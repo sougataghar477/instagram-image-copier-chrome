1e348c925c2ff9a0b162bda67708a3fff1668f51
sougata <sougataghar47@gmail.com>
2025-02-03 14:08:20 +0530

    added json for translation

--- copySrc.js
+++ copySrc.js
@@ -1,4 +1,9 @@
 //Context menu
+fetch('./translation.json')
+  .then(d => d.json())
+  .then(a => console.log(a))
+ 
+
 document.addEventListener("mousedown", (e) => {
   if(e.target.tagName=== "DIV" && (e.target.children.length === 0 || e.target.textContent==="")  && e.button === 2 && e.target.className!==""){
     e.target.className="";
@@ -12,6 +17,7 @@ document.addEventListener("click",(e) => {
     const tooltip = document.createElement("SPAN");
     tooltip.textContent = "copied";
     tooltip.classList.add("tooltip");
+   
     return tooltip;  
   }
   
